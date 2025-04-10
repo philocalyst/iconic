06815b2540b56d7b7b73d6948c6d8fe1f3663112
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-10 08:31:59 -0400

    Initial commit
    Project information and dependencies

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iconic",
+    platforms: [
+        .macOS(.v12)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(path: "/Users/philocalyst/Projects/SwiftVips"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "iconic",
+            dependencies: [
+                "SwiftVips",
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        )
+    ]
+
+)
