976bab0ad62ee0ac8de43718ae4da11009f51433
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-11 12:16:54 -0400

    Added extentions to CGImage, CIImage, and
    To support accurate reading of a CIImage color value. Modified from this
    stackoverflow answer: https://stackoverflow.com/questions/3284185/get-pixel-color-of-uiimage/36236716#36236716
    And modified to change UIColor to CIColor for simplicities sake.

--- Sources/iconic.swift
+++ Sources/iconic.swift
@@ -78,8 +78,180 @@ enum IconAssignmentError: Error, CustomStringConvertible {
 	}
 }
 
+extension CGBitmapInfo {
+	public enum ComponentLayout {
+		case bgra
+		case abgr
+		case argb
+		case rgba
+		case bgr
+		case rgb
+
+		var count: Int {
+			switch self {
+			case .bgr, .rgb: return 3
+			default: return 4
+			}
+		}
+	}
+
+	public var componentLayout: ComponentLayout? {
+		guard let alphaInfo = CGImageAlphaInfo(rawValue: rawValue & Self.alphaInfoMask.rawValue)
+		else { return nil }
+		let isLittleEndian = contains(.byteOrder32Little)
+
+		if alphaInfo == .none {
+			return isLittleEndian ? .bgr : .rgb
+		}
+		let alphaIsFirst =
+			alphaInfo == .premultipliedFirst || alphaInfo == .first || alphaInfo == .noneSkipFirst
+
+		if isLittleEndian {
+			return alphaIsFirst ? .bgra : .abgr
+		} else {
+			return alphaIsFirst ? .argb : .rgba
+		}
+	}
+
+	public var chromaIsPremultipliedByAlpha: Bool {
+		let alphaInfo = CGImageAlphaInfo(rawValue: rawValue & Self.alphaInfoMask.rawValue)
+		return alphaInfo == .premultipliedFirst || alphaInfo == .premultipliedLast
+	}
+}
+
+extension CGColor {
+	public static func create(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) -> CGColor {
+		return CGColor(
+			red: CGFloat(red) / 255,
+			green: CGFloat(green) / 255,
+			blue: CGFloat(blue) / 255,
+			alpha: CGFloat(alpha) / 255
+		)
+	}
+}
+
 extension CIImage {
 	// MARK: - Basic Operations
+	//
+
+	var pixelWidth: Int {
+		return cgImage?.width ?? 0
+	}
+
+	var pixelHeight: Int {
+		return cgImage?.height ?? 0
+	}
+
+	func pixelColor(x: Int, y: Int) -> CGColor {
+		assert(
+			0..<pixelWidth ~= x && 0..<pixelHeight ~= y,
+			"Pixel coordinates are out of bounds"
+		)
+
+		guard
+			let cgImage = cgImage,
+			let data = cgImage.dataProvider?.data,
+			let dataPtr = CFDataGetBytePtr(data),
+			let colorSpaceModel = cgImage.colorSpace?.model,
+			let componentLayout = cgImage.bitmapInfo.componentLayout
+		else {
+			assertionFailure("Could not get a pixel of an image")
+			return CGColor(gray: 0, alpha: 0)
+		}
+
+		assert(
+			colorSpaceModel == .rgb,
+			"The only supported color space model is RGB"
+		)
+		assert(
+			cgImage.bitsPerPixel == 32 || cgImage.bitsPerPixel == 24,
+			"A pixel is expected to be either 4 or 3 bytes in size"
+		)
+
+		let bytesPerRow = cgImage.bytesPerRow
+		let bytesPerPixel = cgImage.bitsPerPixel / 8
+		let pixelOffset = y * bytesPerRow + x * bytesPerPixel
+
+		if componentLayout.count == 4 {
+			let components = (
+				dataPtr[pixelOffset + 0],
+				dataPtr[pixelOffset + 1],
+				dataPtr[pixelOffset + 2],
+				dataPtr[pixelOffset + 3]
+			)
+
+			var alpha: UInt8 = 0
+			var red: UInt8 = 0
+			var green: UInt8 = 0
+			var blue: UInt8 = 0
+
+			switch componentLayout {
+			case .bgra:
+				alpha = components.3
+				red = components.2
+				green = components.1
+				blue = components.0
+			case .abgr:
+				alpha = components.0
+				red = components.3
+				green = components.2
+				blue = components.1
+			case .argb:
+				alpha = components.0
+				red = components.1
+				green = components.2
+				blue = components.3
+			case .rgba:
+				alpha = components.3
+				red = components.0
+				green = components.1
+				blue = components.2
+			default:
+				return CGColor(gray: 0, alpha: 0)
+			}
+
+			/// If chroma components are premultiplied by alpha and the alpha is `0`,
+			/// keep the chroma components to their current values.
+			if cgImage.bitmapInfo.chromaIsPremultipliedByAlpha, alpha != 0 {
+				let invisibleUnitAlpha = 255 / CGFloat(alpha)
+				red = UInt8((CGFloat(red) * invisibleUnitAlpha).rounded())
+				green = UInt8((CGFloat(green) * invisibleUnitAlpha).rounded())
+				blue = UInt8((CGFloat(blue) * invisibleUnitAlpha).rounded())
+			}
+
+			return CGColor.create(red: red, green: green, blue: blue, alpha: alpha)
+
+		} else if componentLayout.count == 3 {
+			let components = (
+				dataPtr[pixelOffset + 0],
+				dataPtr[pixelOffset + 1],
+				dataPtr[pixelOffset + 2]
+			)
+
+			var red: UInt8 = 0
+			var green: UInt8 = 0
+			var blue: UInt8 = 0
+
+			switch componentLayout {
+			case .bgr:
+				red = components.2
+				green = components.1
+				blue = components.0
+			case .rgb:
+				red = components.0
+				green = components.1
+				blue = components.2
+			default:
+				return CGColor(gray: 0, alpha: 0)
+			}
+
+			return CGColor.create(red: red, green: green, blue: blue, alpha: UInt8(255))
+
+		} else {
+			assertionFailure("Unsupported number of pixel components")
+			return CGColor(gray: 0, alpha: 0)
+		}
+	}
 
 	/// Colorizes the image with a specified fill color
 	func fillColorize(color: CIColor) throws -> CIImage {
