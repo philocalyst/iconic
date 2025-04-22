70d568eb8110bb7773f731d735a2224b2fdf0c63
Miles Wirht <regime_skylark.0y@icloud.com>
2025-04-22 16:07:54 -0400

    Removed swiftvips dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(path: "/Users/philocalyst/Projects/SwiftVips"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
